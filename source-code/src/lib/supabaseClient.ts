cbd1e68bf06122606cd21b5bb4b12a6bfe73f097
Lut Lat Aung <lutlataung03@gmail.com>
2025-06-12 22:26:22 +0700

    Update supabaseClient.ts

--- source-code/src/lib/supabaseClient.ts
+++ source-code/src/lib/supabaseClient.ts
@@ -1,5 +1,10 @@
-'use client';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+// src/lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createBrowserSupabaseClient();
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  { auth: { persistSession: false } }   // no localStorage on server
+);
+
 export default supabase;
