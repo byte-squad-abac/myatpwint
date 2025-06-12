cb12563d6c8d06ac4969326d250a5b8e33d28a75
Lut Lat Aung <lutlataung03@gmail.com>
2025-06-12 17:43:02 +0700

    add temp interface and export for detailed book page

---
+++ source-code/src/app/books/[id]/page.tsx
@@ -0,0 +1,10 @@
+// src/app/books/[id]/page.tsx
+
+export default function BookDetailPage() {
+  return (
+    <div>
+      <h1>Book Detail Page</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
