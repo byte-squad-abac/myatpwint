6c77c873555fbe49f80ac4535c55b3fb6ee2cb5c
Wai Yan <waiyanpaing183@gmail.com>
2025-06-27 03:46:40 +0700

    Update manuscript and publisher dashboard UI

---
+++ .next/static/webpack/app/publisher/manuscripts/page.4f4cb184f9a1f469.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/publisher/manuscripts/page",{
+
+/***/ "(app-pages-browser)/./src/app/publisher/manuscripts/manuscripts.css":
+/*!*******************************************************!*\
+  !*** ./src/app/publisher/manuscripts/manuscripts.css ***!
+  \*******************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"ea9fa6388c7a\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvcHVibGlzaGVyL21hbnVzY3JpcHRzL21hbnVzY3JpcHRzLmNzcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIkM6XFxVc2Vyc1xcd2FpeWFcXERlc2t0b3BcXG15YXRwd2ludC1tYWluXFxteWF0cHdpbnQtbWFpblxcc3JjXFxhcHBcXHB1Ymxpc2hlclxcbWFudXNjcmlwdHNcXG1hbnVzY3JpcHRzLmNzcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcImVhOWZhNjM4OGM3YVwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/publisher/manuscripts/manuscripts.css\n"));
+
+/***/ })
+
+});
